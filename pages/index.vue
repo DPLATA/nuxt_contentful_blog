72080be2add02c8e920f8c3186acc618cf84cbb5
Diego Plata <diegogplata@gmail.com>
2021-12-28 13:20:40 -0600

    set image visible in index.vue and change styles file

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,19 @@
    <div class="container">
      <h1 class="title">Plata planet</h1>
      <section v-for="post in posts" :key="post.fields.slug">
-       <!--div
+       <div
          class="image"
          :style="
            `background: url(https:${post.fields.heroImage.fields.file.url}) center center no-repeat`
          "
-       ></div-->
+       ></div>
        <h2 class="title">
          <nuxt-link :to="post.fields.slug">{{ post.fields.title }}</nuxt-link>
        </h2>
-       <p class="author">By {{ post.fields.author.fields.name }}</p>
+       <p class="author">Autor: {{ post.fields.author.fields.name }}</p>
        <p class="description">
          {{ post.fields.description }}<br />
-         <nuxt-link :to="post.fields.slug" class="more">Read more ⟶</nuxt-link>
+         <nuxt-link :to="post.fields.slug" class="more">Leer ⟶</nuxt-link>
        </p>
      </section>
    </div>
