37e0402830dc2e67b8ed777fd09fc2b6e87341ad
Diego Plata <diegogplata@gmail.com>
2021-12-28 11:54:58 -0600

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,83 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt_contentful_blog',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ["~/css/main.css"],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ["~/plugins/contentful", "~/plugins/posts"],
+
+  // Environment variables
+   env: {
+    CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
+    CONTENTFUL_ACCESSTOKEN: process.env.CONTENTFUL_ACCESSTOKEN,
+    CONTENTFUL_ENVIRONMENT: process.env.CONTENTFUL_ENVIRONMENT
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    "@nuxtjs/markdownit"
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  // Markdown init module configuration
+  markdownit: {
+    injected: true
+  },
+
+  generate: {
+    routes() {
+      return Promise.all([
+        client.getEntries({
+          content_type: "blogPost"
+        })
+      ]).then(([blogEntries]) => {
+        return [...blogEntries.items.map(entry => entry.fields.slug)];
+      });
+    }
+  }
+
+}
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+   <div class="container">
+     <h1 class="title">Plata planet</h1>
+     <section v-for="post in posts" :key="post.fields.slug">
+       <!--div
+         class="image"
+         :style="
+           `background: url(https:${post.fields.heroImage.fields.file.url}) center center no-repeat`
+         "
+       ></div-->
+       <h2 class="title">
+         <nuxt-link :to="post.fields.slug">{{ post.fields.title }}</nuxt-link>
+       </h2>
+       <p class="author">By {{ post.fields.author.fields.name }}</p>
+       <p class="description">
+         {{ post.fields.description }}<br />
+         <nuxt-link :to="post.fields.slug" class="more">Read more ⟶</nuxt-link>
+       </p>
+     </section>
+   </div>
+ </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+   return {
+     slug: this.$route.params.slug
+   };
+  },
+  head: {
+   title: "planeta_plata"
+  },
+  computed: {
+   posts() {
+     return this.$store.state.posts;
+   }
+  }
+}
+</script>
