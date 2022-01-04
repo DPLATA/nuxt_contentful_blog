24639940351ad90b4ed45bbba6bd0eb1fffba616
Diego Plata <diegogplata@gmail.com>
2022-01-03 19:38:55 -0600

    fix titles in pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt_contentful_blog',
+    title: 'plata_planet',
     htmlAttrs: {
       lang: 'en',
     },
